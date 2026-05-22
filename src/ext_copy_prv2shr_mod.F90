0ebf547b29f5989a7c19a75e95137e29ee855653
Dafydd Stephenson <dafydd_stephenson@hotmail.com>
2026-05-22 17:23:05 -0600

    convert e*.F90 to free-form; fix bug in compute_extended_bounds

--- src/ext_copy_prv2shr_mod.F90
+++ src/ext_copy_prv2shr_mod.F90
@@ -1,74 +1,74 @@
-      module ext_copy_prv2shr_mod
+module ext_copy_prv2shr_mod
 
-      implicit none
-      private
+  implicit none
+  private
 
-      public :: ext_copy_prv2shr_2d_tile
+  public :: ext_copy_prv2shr_2d_tile
 
-      contains
+contains
 
 #include "cppdefs.opt"
 ! Copy the content of a private array into a shared array over
 ! extended range of indices.
 
-      subroutine ext_copy_prv2shr_2d_tile (istr,iend,jstr,jend, A,B)
-      use param, only: lm, mm, ieast, iwest, jnorth, jsouth
-      use dimensions, only: inode, jnode
-      use dimensions, only: npx, npy
-      implicit none
-      integer istr,iend,jstr,jend, i,j
-      real A(PRIVATE_2D_SCRATCH_ARRAY)
-      real B(GLOBAL_2D_ARRAY)
+  subroutine ext_copy_prv2shr_2d_tile (istr,iend,jstr,jend, A,B)
+    use param, only: lm, mm, ieast, iwest, jnorth, jsouth
+    use dimensions, only: inode, jnode
+    use dimensions, only: npx, npy
+    implicit none
+    integer(kind=4) istr,iend,jstr,jend, i,j
+    real(kind=8) A(PRIVATE_2D_SCRATCH_ARRAY)
+    real(kind=8) B(GLOBAL_2D_ARRAY)
 
-#include "compute_extended_bounds.h"
+# include "compute_extended_bounds.h90"
 
-      do j=jstrR,jendR
-        do i=istrR,iendR
-          B(i,j)=A(i,j)
-        enddo
+    do j=jstrR,jendR
+      do i=istrR,iendR
+        B(i,j)=A(i,j)
       enddo
-      end
+    enddo
+  end subroutine ext_copy_prv2shr_2d_tile
 
 #ifdef SOLVE3D
-      subroutine ext_copy_prv2shr_tile (istr,iend,jstr,jend, A,B,nmax)
-      use param, only: lm, mm, ieast, iwest, jnorth, jsouth
-      use dimensions, only: inode, jnode
-      use dimensions, only: npx, npy
-      implicit none
-      integer istr,iend,jstr,jend, nmax, i,j,k
-      real A(PRIVATE_2D_SCRATCH_ARRAY,nmax)
-      real B(GLOBAL_2D_ARRAY,nmax)
+  subroutine ext_copy_prv2shr_tile (istr,iend,jstr,jend, A,B,nmax)
+    use param, only: lm, mm, ieast, iwest, jnorth, jsouth
+    use dimensions, only: inode, jnode
+    use dimensions, only: npx, npy
+    implicit none
+    integer(kind=4) istr,iend,jstr,jend, nmax, i,j,k
+    real(kind=8) A(PRIVATE_2D_SCRATCH_ARRAY,nmax)
+    real(kind=8) B(GLOBAL_2D_ARRAY,nmax)
 
-# include "compute_extended_bounds.h"
+# include "compute_extended_bounds.h90"
 
-      do k=1,nmax
-        do j=jstrR,jendR
-          do i=istrR,iendR
-            B(i,j,k)=A(i,j,k)
-          enddo
+    do k=1,nmax
+      do j=jstrR,jendR
+        do i=istrR,iendR
+          B(i,j,k)=A(i,j,k)
         enddo
       enddo
-      end
+    enddo
+  end subroutine ext_copy_prv2shr_tile
 
 # define XI_ONLY
-      subroutine ext_copy_prv2shr_1Dslb_tile (istr,iend,j, A,B,nmax)
-      use param, only: lm, mm,  ieast, iwest
-      use dimensions, only: inode, jnode
-      use dimensions, only: npx, npy
-      implicit none
-      integer istr,iend, nmax, i,j,k
-      real A(PRIVATE_1D_SCRATCH_ARRAY,nmax)
-      real B(GLOBAL_2D_ARRAY,nmax)
+  subroutine ext_copy_prv2shr_1Dslb_tile (istr,iend,j, A,B,nmax)
+    use param, only: lm, mm,  ieast, iwest
+    use dimensions, only: inode, jnode
+    use dimensions, only: npx, npy
+    implicit none
+    integer(kind=4) istr,iend, nmax, i,j,k
+    real(kind=8) A(PRIVATE_1D_SCRATCH_ARRAY,nmax)
+    real(kind=8) B(GLOBAL_2D_ARRAY,nmax)
 
-# include "compute_extended_bounds.h"
+# include "compute_extended_bounds.h90"
 
-      do k=1,nmax
-        do i=istrR,iendR
-            B(i,j,k)=A(i,k)
-        enddo
+    do k=1,nmax
+      do i=istrR,iendR
+        B(i,j,k)=A(i,k)
       enddo
-      end
+    enddo
+  end subroutine ext_copy_prv2shr_1Dslb_tile
 # undef XI_ONLY
 #endif
 
-      end module ext_copy_prv2shr_mod
+end module ext_copy_prv2shr_mod
