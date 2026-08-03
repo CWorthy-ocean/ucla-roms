63d1f6893fffead7440c2373c6052159fc6beddb
SamM <smaticka@gmail.com>
2026-08-03 15:34:34 -0400

    add files 4 scorep and trcrs

---
+++ profiling/profiling_namelist/submit_profiling_runs_namelist_trcrs.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+INPUT_DATA_DIR=$ROMS_ROOT/profiling/profiling_namelist/input_data_namelist
+
+# Number of Tracers. NP_TRC iterables:
+NP_TRC=(32 64 100)
+
+#NP_XI, NP_ETA iterables:
+NP_X=(1 2) # 3 6) # 8 12 16 24 32)
+NP_Y=(1 2) # 3 6) # 8 12 16 24 32)
+
+# Create directory for runs
+topdir=profiling_runs_NAMELIST_$(date +%Y%m%d_%H%M%S)
+mkdir $topdir
+cd $topdir && topdir=$(pwd) # get full path
+
+for idT in "${!NP_TRC[@]}"; do
+    for idx in "${!NP_X[@]}"; do
+        cd $topdir
+    
+        trc=${NP_TRC[$idT]}
+        npx=${NP_X[$idx]}
+        npy=${NP_Y[$idx]}
+    
+        # Figure out how many nodes we need:
+        ncpus=$(($npx*npy))
+        if [ $((ncpus % 128)) == 0 ];then
+    	extra_node=0
+        else
+    	extra_node=1
+        fi
+        nnodes=$(($ncpus/128 + $extra_node))
+    
+        echo "Run $idx: npx=$npx and npy=$npy ($ncpus CPUs, $nnodes nodes)"
+        thisdir="${topdir}/run_${idx}_npx_${npx}_npy_${npy}_trcr_${trc}"
+        cp -rpv ../template_configuration_namelist_tracers/ ${thisdir}/
+        cd ${thisdir} && thisdir=$(pwd) #Update var to use full path
+        ln -s $ROMS_ROOT/Work/Makefile .
+        mkdir $thisdir/output
+    
+        mkdir $thisdir/partitioned_inputs/
+        cd $thisdir/partitioned_inputs/
+        ln -s ${INPUT_DATA_DIR}/parent_grid.nc .
+        ln -s ${INPUT_DATA_DIR}/parent_tides.nc .
+        ln -s ${INPUT_DATA_DIR}/parent_surface_bulk_forcing.nc .
+        ln -s ${INPUT_DATA_DIR}/parent_bgc_surface_forcing.nc .
+        ln -s ${INPUT_DATA_DIR}/parent_phys_boundary_forcing_trcrs.nc .
+        ln -s ${INPUT_DATA_DIR}/parent_bgc_boundary_forcing.nc .
+        ln -s ${INPUT_DATA_DIR}/parent_rst_trcrs.20120201000000.nc .
+    
+        cd $thisdir/
+        sed -i -e "s/TEMPLATE_NPX/${npx}/g" namelist.nml
+        sed -i -e "s/TEMPLATE_NPY/${npy}/g" namelist.nml
+        sed -i -e "s/TEMPLATE_ntpass/${trc}/g" namelist.nml
+    
+        sed -i -e "s/TEMPLATE_NPX/${npx}/g" run_with_profiling_anvil.sh
+        sed -i -e "s/TEMPLATE_NPY/${npy}/g" run_with_profiling_anvil.sh
+    
+        sed -i -e "s/TEMPLATE_NNODES/${nnodes}/g" run_with_profiling_anvil.sh
+        sed -i -e "s/TEMPLATE_NCPUS/${ncpus}/g" run_with_profiling_anvil.sh
+    
+        sbatch run_with_profiling_anvil.sh
+    done
+done
